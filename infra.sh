5842bbe5926984380f18c8c7d03521e44a84107d
krishna0852 <krishnatejapalla1@gmail.com>
2024-03-16 17:17:57 +0530

    adjusted space in role creation message

--- infra.sh
+++ infra.sh
@@ -23,7 +23,7 @@ AWS_ACCOUNT_ID=$1
 echo "displaying account id in generateCredes method: $AWS_ACCOUNT_ID"
 
 aws sts assume-role --role-arn "arn:aws:iam::$AWS_ACCOUNT_ID:role/$AWS_ROLE" --role-session-name "terraform-practice" > assume-role-output.txt
-validateCmndStatus $? "rolecreation" 
+validateCmndStatus $? "role creation" 
 export AWS_ACCESS_KEY_ID=`cat assume-role-output.txt | jq -c '.Credentials.AccessKeyId' | tr -d '"' | tr -d ' '`
 validateCmndStatus $? "exporting access_key" 
 export AWS_SECRET_ACCESS_KEY=`cat assume-role-output.txt | jq -c '.Credentials.SecretAccessKey' | tr -d '"' | tr -d ' '`
